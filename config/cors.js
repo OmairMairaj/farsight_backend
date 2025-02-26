8ecd2784265d759cf506cf43078a08d598379116
OmairMairaj <omairmairaj@gmail.com>
2025-02-26 20:51:15 +0500

    OMA 5

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,6 @@
 export function corsHeaders() {
     return {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3001' || 'http://localhost:3000',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         'Access-Control-Allow-Credentials': 'true',
